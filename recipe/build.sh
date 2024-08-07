a95a119091e5ec41e77dbe77bbb6b7edb7a15907
Ian Fitchet <ifitchet@anaconda.com>
2024-08-07 18:21:00 +0100

    basic build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,18 @@
+set -ex
 ./bootstrap.sh
-./configure --datadir=$PREFIX/share/libpostal_data --prefix=$PREFIX
+
+# sse2 seems to be being enabled on ARM64
+conf_args=()
+case "$(uname -m)" in
+arm64|aarch64)
+    conf_args+=( --disable-sse2 )
+    ;;
+esac
+
+./configure \
+    --datadir=$PREFIX/share/libpostal_data \
+    --prefix=$PREFIX \
+    "${conf_args[@]}"
 
 make -j${CPU_COUNT}
 make install
